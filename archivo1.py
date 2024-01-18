2fcc900fcbe1e581194fe5912100b660d52b66ab
Alex18 <diomer.rua.r@gmail.com>
2024-01-18 07:13:48 -0500

    CAmbio para conflicto

--- archivo1.py
+++ archivo1.py
@@ -1,6 +1,6 @@
 nombre = "Diomer"
 edad = 30
-if edad >= 18:
+if edad >= 20:
     print(f" mi nombre es {nombre} y mi edad es {edad}")
 else :
     print(f" mi edad es {edad} y mi nombre es {nombre} ")
