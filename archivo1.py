7089acc6bc31b7b8d6e028d5e6eacafa36812c00
Alex18 <diomer.rua.r@gmail.com>
2024-01-18 07:10:53 -0500

    Este es para la rrama desarrollo

--- archivo1.py
+++ archivo1.py
@@ -4,3 +4,6 @@ if edad >= 18:
     print(f" mi nombre es {nombre} y mi edad es {edad}")
 else :
     print(f" mi edad es {edad} y mi nombre es {nombre} ")
+
+
+print("Cambio a la rama desarrollo python")
